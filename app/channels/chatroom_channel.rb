59e941fb35860945e39a24fe8a6f8d011a5690b1
Matt Doering <ablueplatypus21@gmail.com>
2019-01-24 09:22:15 -0500

    updated files

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -2,11 +2,43 @@ class ChatroomChannel < ApplicationCable::Channel
 
   def subscribed
     # stream_from "some_channel"
-    chatroom = Chatroom.find(params[:chatroom_id])
-    stream_fro chatroom
+    @chatroom = Chatroom.find(params[:id])
+    stream_from "Chatroom-#{@chatroom.id}"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 end
+
+  def join(hash)
+    user = User.find_by(id: hash[:user_id])
+    user.current_room = @chatroom.id
+    user.status = "online"
+
+    user.save
+    unless @chatroom.users.include?(user)
+      @chatroom.users << user
+      @chatroom.save
+      ActionCable.server.broadcast "Chatroom-#{@chatroom.id}", type: "userJoin", user: user
+    end
+    ActionCable.server.broadcast "Chatroom-#{@chatroom.id}", type: "userUpdate", status: "online", user: user.id
+  end
+
+  def leave(hash)
+    user.User.find_by(id: hash[:user_id])
+    @chatroom.user.delete(User.find_by(:id hash[:user_id]))
+    @chatroom.save
+    user.current_room = nil
+    user.status "offline"
+    user.save
+    ActionCable.broadcast "Chatroom-#{@chatroom.id}", type: "userLeft", user: hash[:user_id]
+  end
+
+  def exit(hash)
+    ActionCable.server.broadcast "Chatroom-#{@chatroom.id}", type: "update", status: "offline", user: hash[:user_id]
+  end
+
+  def message(hash)
+    @message = Message.create()
+  end
